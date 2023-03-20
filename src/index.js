69ce18364e4f8ab62ee17c633dfdf80c28fe243e
tumi267 <itu.matlala4@gmail.com>
2023-03-20 15:00:49 +0200

    change for profile

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {HashRouter, Routes, Route} from 'react-router-dom'
+import Home from './Home/Home';
+import Contact from './Contact/Contact'
+import History from './History/History';
+import Reff from './Reff/Reff';
+import About from './About/About'
+import Media from './Media/Media'
+import './index.css'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <HashRouter >
+  <Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/Contact' element={<Contact/>}/>
+  <Route path='/History' element={<History/>}/>
+  <Route path='/Reff' element={<Reff/>}/>
+  <Route path='/About' element={<About/>}/>
+  <Route path='/Media' element={<Media/>}/>
+  </Routes>
+  </HashRouter>
   </React.StrictMode>
 );
 
