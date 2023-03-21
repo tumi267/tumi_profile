dde7a63e1afc61202de6d44160e4acc60d28da8b
tumi267 <itu.matlala4@gmail.com>
2023-03-21 23:29:45 +0200

    site funtion added

--- src/Sites/Sites.js
+++ src/Sites/Sites.js
@@ -7,30 +7,50 @@ function Sites() {
     <div >
     <h1 className='header'>Sites</h1>
     <div className='site_contain'>
-    <div>
+   
+    <div className='frame_perant' onClick={()=>{
+      window.location.replace("https://tumi267.github.io/panel_beter_exapmle/")
+    }}>
+    <h2 className='frame_perant_link'>click to see full site</h2>
     <iframe className='site' src="https://tumi267.github.io/panel_beter_exapmle/" 
     scrolling="no" 
     title='panel-beter'
     height='300px'
     ></iframe>
+    </div>
+    <div className='frame_perant' onClick={()=>{
+      window.location.replace("https://tumi267.github.io/barber/")
+    }}>
+    <h2 className='frame_perant_link'>click to see full site</h2>
     <iframe className='site' src="https://tumi267.github.io/barber/" 
     scrolling="no" 
     title='barber'
     height='300px'
     ></iframe>
+    </div>
+    <div className='frame_perant' onClick={()=>{
+      window.location.replace("https://tumi267.github.io/jozi/")
+    }}>
+    <h2 className='frame_perant_link'>click to see full site</h2>
     <iframe className='site' src="https://tumi267.github.io/jozi/" 
     scrolling="no" 
     title='jozi' 
     height='300px'
     ></iframe>
+    </div>
+    <div className='frame_perant' onClick={()=>{
+      window.location.replace("https://tumi267.github.io/tesla-clone/")
+    }}>
+    <h2 className='frame_perant_link'>click to see full site</h2>
     <iframe className='site' src="https://tumi267.github.io/tesla-clone/" 
     scrolling="no" 
     title='telsa clone' 
     height='300px'
     ></iframe>
     </div>
-
     </div>
+
+    
     </div>
   )
 }
